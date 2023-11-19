1f086d8332d2220af7529c75948a2fbc57f95329
GregorPolegeg <gregor.poleg3g@gmail.com>
2023-11-19 17:30:48 +0100

    UserNavigationUpdate

--- src/components/navigation/userNavigation.tsx
+++ src/components/navigation/userNavigation.tsx
@@ -1,101 +1,76 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import Link from 'next/link';
-import { AiOutlineClose } from 'react-icons/ai'
+import Link from "next/link";
+import { AiOutlineAppstore, AiOutlineBranches, AiOutlineClose } from "react-icons/ai";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { IoHomeOutline } from "react-icons/io5";
+import { TbMessages } from "react-icons/tb";
 
-const navContent = {
-  User: [{name:"Home",route:"/"}, {name:"About",route:"/"}, {name:"Reviews",route:"/"}, {name:"Plans",route:"/"}, {name:"Contacts",route:"/"}],
-  LogedUser: [{name:"Messages",route:"/messages/t"},{name:"Form",route:"/messages/t"},{name:"Support",route:"/messages/t"}, {name:"Contacts",route:"/messages/t"}],
-};
 
+enum SelectedProps {
+  home,
+  messages,
+  o1,
+  o2,
+}
 
 const UserNavigation = () => {
-  const { data: sessionData } = useSession(); 
+  const { data: sessionData } = useSession();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
-  let userNavItems = navContent["User"];
+  const [selectedItem, setSelectedItem] = useState<SelectedProps>();
 
-  if(sessionData?.user){
-    userNavItems = navContent["LogedUser"];
-  }
 
+  const normalClass = "hover:bg-zinc-300 rounded-lg py-3 md:px-10 md:py-3 flex-1 flex justify-center";
   return (
     <>
-      <nav className="px-7 shadow-sm md:py-0 fixed top-0 flex w-full items-center justify-between bg-white md:flex-row flex-row">
+      <nav className="fixed bg-zinc-900 bg-opacity-10 bottom-0 md:top-0 md:bottom-auto flex w-full flex-row items-center justify-center md:justify-between md:bg-white md:px-7 shadow-sm md:flex-row md:py-0">
         {/* Logo */}
-        <div className="flex p-1">
-          <Image
-            width={56}
-            height={56}
-            src="/images/logo.png"
-            alt="Logo"
-          />
+        <div className="hidden md:flex ">
+          <Image width={55} height={55} src="/images/logo.png" alt="Logo" />
         </div>
 
         {/* Desktop Menu */}
-        <ul className="hidden md:flex">
-          {userNavItems.map((item, index) => (
-            <li key={index} className="transition duration-150 hover:bg-gray-300 hover:cursor-pointer text-l p-5 font-normal">
-              <Link href={item.route}>{item.name}</Link>
-            </li>
-          ))}
-        </ul>
-
-        {/* Mobile Menu Button */}
-        <div className="md:hidden flex items-center justify-end">
-          <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-            </svg>
-          </button>
+        <div className="rounded-md w-full md:w-auto no-highlight flex text-[25px] md:text-2xl">
+          <Link
+            href="/"
+            className={`${normalClass}`}
+            onClick={() => setSelectedItem(SelectedProps.home)}
+          >
+            <IoHomeOutline />
+          </Link>
+          <Link
+            href="/messages/t"
+            className={`${normalClass}`}
+            onClick={() => setSelectedItem(SelectedProps.messages)}
+          >
+            <TbMessages />
+          </Link>
+          <Link
+            href="/"
+            className={`${normalClass}`}
+            onClick={() => setSelectedItem(SelectedProps.o1)}
+          >
+            <AiOutlineAppstore />
+          </Link>
+          <Link
+            href="/"
+            className={`${normalClass}`}
+            onClick={() => setSelectedItem(SelectedProps.o2)}
+          >
+            <AiOutlineBranches />
+          </Link>
         </div>
 
         {/* Desktop Login */}
-        <div className="hidden md:flex text-xl font-bold">
-          {!sessionData? (<Link href="/login">Login</Link>): (<button onClick={() => void signOut() }>{sessionData.user.name}</button>)}
-        </div>
-
-        {/* Mobile Menu */}
-        {isMenuOpen && (
-          <div className="fixed top-0 left-0 w-full h-full bg-white p-5 transition-transform transform-gpu z-20">
-            {/* Close Button */}
-            <button onClick={() => setIsMenuOpen(false)} className="text-3xl absolute top-4 right-4">
-              <AiOutlineClose/>
+        <div className="hidden text-xl font-bold md:flex">
+          {!sessionData ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <button onClick={() => void signOut()}>
+              {sessionData.user.name}
             </button>
-
-            {/* Logo */}
-            <div className="flex justify-start items-center mb-8">
-              <Image
-                width={56}
-                height={56}
-                src="https://cdn.worldvectorlogo.com/logos/sc-spelle-venhaus.svg"
-                alt="Logo"
-              />
-            </div>
-
-            {/* Menu Items */}
-            <ul className="mt-8 flex flex-col items-center">
-              {userNavItems.map((item, index) => (
-                <li key={index} className="transition text-center w-full duration-150 hover:bg-gray-300 hover:cursor-pointer text-l py-2 font-bold">
-                  <Link href={item.route}>{item.name}</Link>
-                </li>
-              ))}
-            </ul>
-
-            {/* Login */}
-            <div className="flex justify-center text-center mt-8 text-l font-normal">
-            {!sessionData? (
-              <div>
-                <Link href="/login">Login</Link>
-                <p className="px-5">|</p>
-                <Link href="/register">Register</Link>
-              </div>
-              ) : (<button onClick={() => void signOut()}>LogOut</button>)}
-              
-            </div>
-          </div>
-        )}
+          )}
+        </div>
       </nav>
     </>
   );
